5ce91143fdfad8c72249843b20e2a53c2cf755b0
Edwin Sulaiman <edwinsulaiman20@gmail.com>
2023-09-01 04:04:23 +0700

    feat: change authentication route

--- app/routes/_authentication.login.tsx
+++ app/routes/_authentication.login.tsx
@@ -1,14 +1,18 @@
 import { Button, Container, Stack, TextField } from "@mui/material";
+import { V2_MetaFunction } from "@remix-run/react";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "Masuk - LocalPlace" }];
+};
 
 export default function Page() {
   return (
     <Container sx={{ display: "flex", flex: 1, alignItems: "center" }} maxWidth="xs">
       <Stack flex={1} gap={"16px"}>
-        <TextField label="Nama Lengkap"></TextField>
         <TextField label="Alamat Email" type="email"></TextField>
         <TextField label="Password" type="password"></TextField>
         <Button type="submit" variant="contained" disableElevation>
-          Daftar
+          Masuk
         </Button>
       </Stack>
     </Container>
--- app/routes/_authentication.register.tsx
+++ app/routes/_authentication.register.tsx
@@ -1,13 +1,19 @@
 import { Button, Container, Stack, TextField } from "@mui/material";
+import { V2_MetaFunction } from "@remix-run/react";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "Daftar - LocalPlace" }];
+};
 
 export default function Page() {
   return (
     <Container sx={{ display: "flex", flex: 1, alignItems: "center" }} maxWidth="xs">
       <Stack flex={1} gap={"16px"}>
+        <TextField label="Nama Lengkap"></TextField>
         <TextField label="Alamat Email" type="email"></TextField>
         <TextField label="Password" type="password"></TextField>
         <Button type="submit" variant="contained" disableElevation>
-          Masuk
+          Daftar
         </Button>
       </Stack>
     </Container>
