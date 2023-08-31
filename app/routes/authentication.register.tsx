9adfd5b1897293901585bce823665a88590f8ab2
Edwin Sulaiman <edwinsulaiman20@gmail.com>
2023-09-01 03:02:39 +0700

    faet: add register page

---
+++ app/routes/authentication.register.tsx
@@ -0,0 +1,16 @@
+import { Button, Container, Stack, TextField } from "@mui/material";
+
+export default function Page() {
+  return (
+    <Container sx={{ display: "flex", flex: 1, alignItems: "center" }} maxWidth="xs">
+      <Stack flex={1} gap={"16px"}>
+        <TextField label="Nama Lengkap"></TextField>
+        <TextField label="Alamat Email" type="email"></TextField>
+        <TextField label="Password" type="password"></TextField>
+        <Button type="submit" variant="contained" disableElevation>
+          Daftar
+        </Button>
+      </Stack>
+    </Container>
+  );
+}
